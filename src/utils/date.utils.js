fb606ffdb0caa586fff9b72a4dee1556dfd796bf
anisma <anismaghfiroh63@gmail.com>
2020-11-17 10:55:36 +0700

    adding date.utils which convert International Date into Indonesian Date and get next 2 weeks date

---
+++ src/utils/date.utils.js
@@ -0,0 +1,81 @@
+const date = new Date();
+
+export const getDate= (date) => {
+   return date.getDate();
+} 
+
+export const getDay = (date) => {
+   const day = date.getDay();
+   switch (day) {
+      case 0:
+         return "MIN"
+      case 1:
+         return "SEN"
+      case 2:
+         return "SEL"
+      case 3:
+         return "RAB"
+      case 4:
+         return "KAM"
+      case 5:
+         return "JUM"
+      case 6:
+         return "SAB"
+      default:
+         break;
+   }
+}
+
+export const getMonth = (date) => {
+   const month = date.getMonth();
+   switch (month) {
+      case 0:
+         return "Januari"
+      case 1:
+         return "Februari"
+      case 2:
+         return "Maret"
+      case 3:
+         return "April"
+      case 4:
+         return "Mei"
+      case 5:
+         return "Juni"
+      case 6:
+         return "Juli"
+      case 7:
+         return "Agustus"
+      case 8:
+         return "September"
+      case 9:
+         return "Oktober"
+      case 10:
+         return "November"
+      case 11:
+         return "Desember"
+      default:
+         break;
+   }
+}
+
+export const getTodaysDate = () => {
+   const date = new Date();
+   const _date = date.getDate();
+   const _day = getDay(date);
+   const _month = getMonth(date);
+   const _year = date.getFullYear();
+
+   return `${_day}, ${_date} ${_month} ${_year}`
+}
+
+export const getNext2Weeks = () => {
+   const twoWeeks = [];
+  
+   for (let i = 0; i < 14; i++) {
+      let date = new Date();
+      date.setDate(date.getDate() + i);
+      twoWeeks.push({ fullDate: date, day: getDay(date), date: date.getDate() })
+   }
+
+   return twoWeeks
+}
